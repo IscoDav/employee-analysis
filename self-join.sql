af73dab4a5a24c67792fea8e346c1202a13fdbd6
Isco Davronov <davronovisco@gmail.com>
2022-03-22 14:57:26 +0500

    New file is add for the topic self-joins

---
+++ self-join.sql
@@ -0,0 +1,16 @@
+
+# Question : When is it appropriate to use self-joins?
+# Answer:  When a column in a table is referenced in the same table
+
+/*
+From the emp_manager table, extract the record
+data only of those employees who are
+managers as well.
+*/
+
+SELECT DISTINCT
+    e1.*
+FROM
+    emp_manager e1
+        JOIN
+    emp_manager e2 ON e1.emp_no = e2.manager_no;
